81e274a416e62f3535901c36041f37e72c5a02fe
Vitor_Augusto_Rodrigues <131215606+0072023@users.noreply.github.com>
2024-09-18 13:39:02 -0300

    Add files via upload

---
+++ Variables and Assignments.js
@@ -0,0 +1,16 @@
+//variables and assignments
+let x;
+x = 10;
+console.log(x)
+
+let fullName = 'Vitor Augusto Rodrigues de Campos'
+let age = 20
+let isStudent = true
+
+document.getElementById("p1") .textContent = `Your full name is: ${fullName}`;
+document.getElementById("p2") .textContent = `Your age is: ${age}`
+document.getElementById("p3") .textContent = `Enrolled: ${isStudent}`
+
+console.log(typeof fullName)
+console.log(typeof age)
+console.log(typeof isStudent)
